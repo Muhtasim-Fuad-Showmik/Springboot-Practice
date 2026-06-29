723a9a05074db4675eef5ab48da8c804e46006c5
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2026-06-29 14:45:36 +0600

    feat: create API for returning a list of students from a GET request

--- src/main/java/com/magnus/store/StudentController.java
+++ src/main/java/com/magnus/store/StudentController.java
@@ -3,15 +3,39 @@ package com.magnus.store;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("student")
     public Student getStudent() {
-        Student student = new Student(
+        return new Student(
                 1,
                 "Muhtasim",
-                "Fuad"
-        );
-        return student;
+                "Fuad");
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(
+                1,
+                "Muhtasim",
+                "Fuad"));
+        students.add(new Student(
+                2,
+                "John",
+                "Doe"));
+        students.add(new Student(
+                3,
+                "Jane",
+                "Doe"));
+        students.add(new Student(
+                4,
+                "Bob",
+                "Smith"));
+
+        return students;
     }
 }
