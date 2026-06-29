4df02635f8902cbe206bcb9970c4e8c9fc042613
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2026-06-29 14:44:59 +0600

    doc: add comments to differentiate annotations for template based generic requests on HomeController

--- src/main/java/com/magnus/store/HomeController.java
+++ src/main/java/com/magnus/store/HomeController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // Used for controllers that are meant to return template files with filename on
+            // returned string
 public class HomeController {
     @Value("${spring.application.name}")
     private String appName;
 
-    @RequestMapping("/")
+    @RequestMapping("/") // Generic request matching for GET, POST, PUT, etc, everything
     public String index() {
         System.out.println("App Name: " + appName);
         return "index.html";
