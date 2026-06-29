e6c27d3959eb8389963291016faa65f33851f5f6
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2026-06-29 16:45:51 +0600

    refactor: refactor Student Controller to send response using ResponseEntity with proper HTTP Status codes

--- src/main/java/com/magnus/store/StudentController.java
+++ src/main/java/com/magnus/store/StudentController.java
@@ -1,6 +1,7 @@
 package com.magnus.store;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,15 +10,21 @@ import java.util.List;
 @RestController
 public class StudentController {
     @GetMapping("student")
-    public Student getStudent() {
-        return new Student(
+    public ResponseEntity<Student> getStudent() {
+        Student student = new Student(
                 1,
                 "Muhtasim",
                 "Fuad");
+
+        // Both approach from below are okay
+        // return new ResponseEntity<>(student, HttpStatus.OK);
+        return ResponseEntity.ok()
+                .header("Custom-Header", "Custom-Value")
+                .body(student);
     }
 
     @GetMapping("students")
-    public List<Student> getStudents() {
+    public ResponseEntity<List<Student>> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(
                 1,
@@ -36,40 +43,43 @@ public class StudentController {
                 "Bob",
                 "Smith"));
 
-        return students;
+        return ResponseEntity.ok(students);
     }
 
     @GetMapping("students/{id}") // id here is a URI template variable
-    public Student studentPathVariable(@PathVariable("id") int studentId) {
+    public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId) {
         // @PathVariable("id") int studentId is being used to bind studentId to id.
-        // When both variables are named the same, the "id" no longer needs to be specified within the parentheses
-        return new Student(studentId, "Muhtasim", "Fuad");
+        // When both variables are named the same, the "id" no longer needs to be
+        // specified within the parentheses
+        Student student = new Student(studentId, "Muhtasim", "Fuad");
+        return ResponseEntity.ok(student);
     }
 
     @GetMapping("students/query")
-    public Student studentRequestVariable(@RequestParam int id) {
-        return new Student(id, "Muhtasim", "Fuad");
+    public ResponseEntity<Student> studentRequestVariable(@RequestParam int id) {
+        Student student = new Student(id, "Muhtasim", "Fuad");
+        return ResponseEntity.ok(student);
     }
 
     @PostMapping("students/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student) {
+    // @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         System.out.println("ID: " + student.getId());
         System.out.println("First Name: " + student.getFirstName());
         System.out.println("Last Name: " + student.getLastName());
-        return student;
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @PutMapping("students/{id}/update")
-    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
         System.out.println("First Name: " + student.getFirstName());
         System.out.println("Last Name: " + student.getLastName());
-        return student;
+        return ResponseEntity.ok(student);
     }
 
     @DeleteMapping("students/{id}/delete")
-    public String deleteStudent(@PathVariable("id") int studentId) {
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId) {
         System.out.println(studentId);
-        return "Student deleted successfully!";
+        return ResponseEntity.ok("Student deleted successfully!");
     }
 }
