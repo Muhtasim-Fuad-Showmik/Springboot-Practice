8865dbf36eac436d58db3c1e5ca79776b9cc2c66
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2026-06-29 16:55:12 +0600

    refactor: update StudentController to use base url instead of repeating "student" every time

--- src/main/java/com/magnus/store/StudentController.java
+++ src/main/java/com/magnus/store/StudentController.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
     @GetMapping("student")
     public ResponseEntity<Student> getStudent() {
@@ -23,7 +24,7 @@ public class StudentController {
                 .body(student);
     }
 
-    @GetMapping("students")
+    @GetMapping()
     public ResponseEntity<List<Student>> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(
@@ -46,7 +47,7 @@ public class StudentController {
         return ResponseEntity.ok(students);
     }
 
-    @GetMapping("students/{id}") // id here is a URI template variable
+    @GetMapping("{id}") // id here is a URI template variable
     public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId) {
         // @PathVariable("id") int studentId is being used to bind studentId to id.
         // When both variables are named the same, the "id" no longer needs to be
@@ -55,13 +56,13 @@ public class StudentController {
         return ResponseEntity.ok(student);
     }
 
-    @GetMapping("students/query")
+    @GetMapping("query")
     public ResponseEntity<Student> studentRequestVariable(@RequestParam int id) {
         Student student = new Student(id, "Muhtasim", "Fuad");
         return ResponseEntity.ok(student);
     }
 
-    @PostMapping("students/create")
+    @PostMapping("create")
     // @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         System.out.println("ID: " + student.getId());
@@ -70,14 +71,14 @@ public class StudentController {
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
-    @PutMapping("students/{id}/update")
+    @PutMapping("{id}/update")
     public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
         System.out.println("First Name: " + student.getFirstName());
         System.out.println("Last Name: " + student.getLastName());
         return ResponseEntity.ok(student);
     }
 
-    @DeleteMapping("students/{id}/delete")
+    @DeleteMapping("{id}/delete")
     public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId) {
         System.out.println(studentId);
         return ResponseEntity.ok("Student deleted successfully!");
