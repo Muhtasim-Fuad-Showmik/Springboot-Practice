e0918871fa0b1806106a755bc3b8807bf1c41658
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2026-06-30 12:34:41 +0600

    feat: add DTO and mapper for the employee entity

---
+++ src/main/java/com/magnus/employeemanagement/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.magnus.employeemanagement.mapper;
+
+import com.magnus.employeemanagement.dto.EmployeeDto;
+import com.magnus.employeemanagement.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
